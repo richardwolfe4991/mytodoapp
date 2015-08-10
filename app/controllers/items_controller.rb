a1b5e68dd5134377b7423e954dee8b41d6554f19
richardwolfe4991 <richardwolfe4991@gmail.com>
2015-08-10 09:20:30 -0700

    Backend of application fully functional

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
 
-	def index
+	def new
+		@item = Item.new
+		@list = List.find params[:list_id]
+	end
+
+	def create
+		@list = List.find(params[:list_id])
+		@item = Item.new(params[:item])
+		@item.list_id = @list.id
+		logger.error("HOLY: #{@list.id}")
+		if @item.save
+			redirect_to list_path(:id => @list.id)
+		else
+			redirect_to new_list_item_path
+		end
+	end
+
+	def show
 	end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,38 @@ class ListsController < ApplicationController
 		@list_array = List.all
 	end
 
+	def new
+		@list = List.new
+	end
+
+	def create
+		@list = List.new(params[:list])
+		if @list.save
+			redirect_to root_path
+		else
+			redirect_to new_list_path
+		end
+	end
+
+	def edit
+		@list = List.find(params[:id])
+	end
+
+	def update
+		@list = List.find(params[:id])
+		if @list.update_attributes(params[:list])
+			redirect_to list_path
+		else 
+			redirect_to edit_list_path
+		end
+	end
+
+	def destroy
+		@list_destroy = List.find(params[:id]).destroy
+		redirect_to root_path
+	end
+
+
 	def show
 		@list_view = List.find(params[:id]) 
 		@item_array = Item.where(["list_id = :list_id", {list_id: List.find(params[:id])}])
